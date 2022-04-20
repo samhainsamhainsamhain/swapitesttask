04d88f6508fd1414f9ccf543ebd91f672e88cb40
samhainsamhainsamhain <83346225+samhainsamhainsamhain@users.noreply.github.com>
2022-04-20 09:48:56 +0300

    added search results outputing logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,37 @@ import { useState } from "react";
 
 import SearchBox from "./components/search/SearchBox";
 import Character from "./components/character/Character";
-import { PeopleSearchResult } from "./swapi/swapiInterfaces";
+import { People, PeopleSearchResult } from "./swapi/swapiInterfaces";
 
 import "./App.css";
+import SearchResults from "./components/searchResults/SearchResults";
 
 function App() {
+  const [showedCharacter, setShowedCharacter] = useState<People | null>(null);
   const [searchResult, setSearchResult] = useState<PeopleSearchResult | null>(
     null
   );
 
   function getSearchResult(queryResult: PeopleSearchResult) {
+    setShowedCharacter(null);
     setSearchResult(queryResult);
+  }
+
+  function showCharacter(character: People) {
+    console.log(character);
+    setShowedCharacter(character);
   }
 
   return (
     <div className="App">
       <SearchBox onSearchResultReceived={getSearchResult} />
+      {searchResult === null && <h2>Swapi Character Searcher</h2>}
+      {searchResult !== null && showedCharacter === null && (
+        <SearchResults
+          results={searchResult}
+          resultClickHandler={showCharacter}
+        />
+      )}
       <Character />
     </div>
   );
