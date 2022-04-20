7b72999f5c1a710726b2df47c525248542068f72
samhainsamhainsamhain <83346225+samhainsamhainsamhain@users.noreply.github.com>
2022-04-20 09:30:01 +0300

    created search requests functionality

--- src/components/search/SearchBox.tsx
+++ src/components/search/SearchBox.tsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-import classes from "./SearchBox.module.css"
+import { swapiFetch } from "../../swapi/swapiFetch";
+import { PeopleSearchResult } from "../../swapi/swapiInterfaces";
 
-export default function searchBox() {
+import classes from "./SearchBox.module.css";
+
+interface SearchBoxProps {
+  onSearchResultReceived: (searchResult: PeopleSearchResult) => void;
+}
+
+export default function SearchBox(props: SearchBoxProps) {
+  const [query, setQuery] = useState("");
+
+  function queryChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
+    setQuery(event.target.value);
+  }
+
+  async function getSearchResult(event: React.ChangeEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const queryResult = (await swapiFetch(
+      `people/?search=${query}`
+    )) as PeopleSearchResult;
+    props.onSearchResultReceived(queryResult);
+  }
+
   return (
     <header className={classes.searchBox}>
-      <form>
+      <form onSubmit={getSearchResult}>
         <input
           placeholder="Search for a character"
+          onChange={queryChangeHandler}
+          value={query}
         />
         <button type="submit">Search</button>
       </form>
